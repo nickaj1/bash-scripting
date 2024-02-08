52b139e3e1cf29aa6dcf96a98f80d58a2f0168ef
nickaj1 <nickaj1505@gmail.com>
2024-02-08 11:23:56 -0500

    arrays for storing and accessing dataset using csv format

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A simple Bash script containing a conditional statement to handle the following 
 
 # Second Phase
 A Bash script that performs basic arithmetic calculations on two integers entered by the user. With logical comparisons to determine which calculation leads to the greatest result.
+
+# Third Phase
+A report based on a supplied dataset using the CSV format.  Extracting the columns of the dataset into separate arrays and creating a new column using arithmetic and array logic. Finally, combining the dataset with the new column and save the resulting report as a CSV file.
---
+++ arrays.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# Imported csv file
+csv_file="./arrays_table.csv"
+
+
+# Parse table columns into 3 arrays
+column_0=($(cut -d "," -f 1 $csv_file))
+column_1=($(cut -d "," -f 2 $csv_file))
+column_2=($(cut -d "," -f 3 $csv_file))
+
+
+# Print first array
+echo "Displaying the first column:"
+echo "${column_0[@]}"
+
+# Create a new array as the difference of columns 1 and 2 
+# initialize array with header
+column_3=("column_3")
+
+
+# get the number of lines in each column
+nlines=$(cat $csv_file | wc -l)
+echo "There are $nlines lines in the file"
+
+
+# populate the array
+for ((i=1; i<$nlines; i++)); do
+  column_3[$i]=$((column_2[$i] - column_1[$i]))
+done  
+
+echo "${column_3[@]}"
+
+
+# Combine the new array with the csv file
+# first write the new array to file
+# initialize the file with a header
+echo "${column_3[0]}" > column_3.txt
+
+for ((i=1; i<nlines; i++)); do
+  echo "${column_3[$i]}" >> column_3.txt
+done
+
+# Save report as a CSV file.
+paste -d "," $csv_file column_3.txt > report.csv
