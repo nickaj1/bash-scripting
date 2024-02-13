50557c975f6b513b98195dce6e3eaac86ccb1671
nickaj1 <nickaj1505@gmail.com>
2024-02-13 16:29:43 -0500

    extract,transform and load data

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ A Bash script that performs basic arithmetic calculations on two integers entere
 
 # Third Phase
 A report based on a supplied dataset using the CSV format.  Extracting the columns of the dataset into separate arrays and creating a new column using arithmetic and array logic. Finally, combining the dataset with the new column and save the resulting report as a CSV file.
+
+
+# Forth Phase
+In this phase, i will:
+Initialize log file
+Write a Bash script to download, extract, and load raw data into a report
+Add some basic analytics to the report
+Schedule the report to update daily
+Measure and report on historical forecasting accuracy
---
+++ rx_poc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+city="Casablanca"
+
+# Obtaining the weather information for Casablanca
+curl -s wttr.in/$city?T --output weather_report
+
+
+#To extract Current Temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+
+
+
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+
+# Store the current hour, day, month, and year in corresponding shell variables
+hour=$(TZ='Morocco/Casablanca' date -u +%H) 
+day=$(TZ='Morocco/Casablanca' date -u +%d) 
+month=$(TZ='Morocco/Casablanca' date +%m)
+year=$(TZ='Morocco/Casablanca' date +%Y)
+
+
